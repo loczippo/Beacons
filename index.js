7c2029da9876ee825e89ce489fe028d0e1f33601
loczippo <kimloc1000@gmail.com>
2022-05-28 14:14:47 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const { dirname } = require("path");
+const res = require("express/lib/response");
+
+var bodyParser = require("body-parser");
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//Routes
+
+app.get('/', (req, res) => {
+  res.render('Index')
+})
+
+
+
+//Handle errors
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  const status = err.status || 500;
+  res.status(status);
+  res.send("error");
+});
+
+app.listen(port, function (error) {
+  if (error) {
+    console.log("Something went wrong");
+  }
+  console.log("server is running port:  " + port);
+});
