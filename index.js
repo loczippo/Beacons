edecab0d16e6a5a6fe42f656955e4223d50db03b
loczippo <kimloc1000@gmail.com>
2022-06-01 12:34:42 +0700

    asd

--- index.js
+++ index.js
@@ -5,6 +5,40 @@ const res = require("express/lib/response");
 
 var bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+// INIT DB
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://admin:Aa123123@cluster0.h16kj.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Connected!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// CONNECT DB
+connect();
+
+// DEFINED MODELS
+
+const Schema = mongoose.Schema;
+const Item = new Schema(
+  {
+    name: { type: String },
+    image: { type: String },
+    url: { type: String },
+    position: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// CALL MODELS
+const MyModel = mongoose.model("Item", Item);
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -13,16 +47,50 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Routes
+// ===> Routes
 
+// VIEW ADD ITEM
+app.get("/addItem", (req, res) => {
+  res.render("AddItem");
+});
+
+// POST ADD ITEM
+
+app.post("/addItem/stored", (req, res) => {
+  const formData = req.body;
+  MyModel.findOne()
+    .sort({ $natural: -1 })
+    .limit(1)
+    .exec(function (err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (res == null) {
+          formData.position = "left";
+        } else {
+          if (res.position == "left") formData.position = "right";
+          else if (res.position == "right") formData.position = "left";
+        }
+
+        const item = new MyModel(formData);
+        item.save();
+      }
+    });
+  res.send("Success!");
+});
+
+// HOME
 app.get("/", (req, res) => {
-  res.render("Index");
+  MyModel.find({}, function (err, data) {
+    if (!err) res.render("Index", { data });
+    else res.status(400).json({ error: "ERROR!!" });
+  });
 });
 
 app.get("/image", (req, res) => {
   var fullUrl = req.originalUrl;
-  var imgUrl = fullUrl.slice(fullUrl.search('=')+1);
-  res.render('ImageViewer', {data: imgUrl})
+  var imgUrl = fullUrl.slice(fullUrl.search("=") + 1);
+  res.render("ImageViewer", { data: imgUrl });
 });
 
 //Handle errors
